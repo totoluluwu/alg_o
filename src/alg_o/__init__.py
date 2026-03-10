272f9aedf01cf383eeaa8c2cbf2ee695bd7e04d5
Axel <drafter-bicycle.0t@icloud.com>
2026-03-10 16:25:53 +0100

    [UPDATE BASE API]

---
+++ src/alg_o/__init__.py
@@ -0,0 +1,18 @@
+from .exception import (
+    AlgOError,
+    BenchmarkError,
+    GenerationError,
+    InvalidAnnotationError,
+    RegressionError,
+    UnsupportedTypeError,
+)
+
+
+__all__ = [
+    "AlgOError",
+    "GenerationError",
+    "UnsupportedTypeError",
+    "InvalidAnnotationError",
+    "BenchmarkError",
+    "RegressionError",
+]
